b7674d8751fe7233171fef3a405de80f024776d1
Casper Anisimow <81159246+anisthepooh@users.noreply.github.com>
2025-06-11 17:57:17 +0200

    add correct reciver

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url = "http://example.com/login"
-    mail(to: [ "delivered@resend.dev" ], subject: "hello world")
+    mail(to: [ @user.email_address ], subject: "Welcome to Bite Habit")
   end
 end
