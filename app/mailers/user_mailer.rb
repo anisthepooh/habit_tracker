c0408ad7facaa91dac952a096cf63a0220bc2f74
Casper Anisimow <81159246+anisthepooh@users.noreply.github.com>
2025-06-12 18:47:54 +0200

    wi0p

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
-    @url = "http://example.com/login"
     mail(to: [ @user.email_address ], subject: "Welcome to Bite Habit")
   end
 end
