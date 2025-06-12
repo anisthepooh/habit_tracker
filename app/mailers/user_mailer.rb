70f8b9945f12556be5825be43eb55ccdff5f53b5
Casper Anisimow <81159246+anisthepooh@users.noreply.github.com>
2025-06-12 12:56:14 +0200

    removed resend example code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,4 @@
 class UserMailer < ApplicationMailer
-  default from: "Acme <onboarding@resend.dev>" # this domain must be verified with Resend
   def welcome_email
     @user = params[:user]
     @url = "http://example.com/login"
